f2d6e81935f5482aa7ea804ee88b4706711226bd
Spencer Chan <spencer.chan.25@ucl.ac.uk>
2025-10-21 13:23:01 +0100

    Fourth Committ

--- testingtesting123.R
+++ testingtesting123.R
@@ -8,3 +8,18 @@ install.packages("usethis")
 library(usethis)
 
 use_github()
+
+z <- 12
+
+usethis::git_remotes()
+
+usethis::gh_token_help()
+
+create_github_token()
+
+install.packages("gitcreds")
+
+library(gitcreds)
+
+gitcreds_set()
+3
